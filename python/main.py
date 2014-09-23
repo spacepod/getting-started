b7b0839bc6532bfb476240e91e9b4ad18ddf2ac3
cassiedoll <cassandradoll@gmail.com>
2014-09-23 15:57:51 -0700

    Add variant calling example

--- python/main.py
+++ python/main.py
@@ -53,28 +53,30 @@ service = build('genomics', 'v1beta', http=http)
 
 
 #
-# This example gets the read bases for NA12878 at specific a position
+# This example gets the read bases for a sample at specific a position
 #
 dataset_id = 376902546192 # This is the 1000 Genomes dataset ID
+sample = 'NA12872'
 reference_name = '22'
-reference_position = 51005354
+reference_position = 51003836
 
-# 1. First find the readset ID for NA12878
+
+# 1. First find the readset ID for the sample
 request = service.readsets().search(
-  body={'datasetIds': [dataset_id], 'name': 'NA12878'},
+  body={'datasetIds': [dataset_id], 'name': sample},
   fields='readsets(id)')
 readsets = request.execute().get('readsets', [])
 if len(readsets) != 1:
-  raise Exception('Searching for NA12878 didn\'t return '
-                  'the right number of results')
+  raise Exception('Searching for %s didn\'t return '
+                  'the right number of readsets' % sample)
 
-na12878 = readsets[0]['id']
+readset_id = readsets[0]['id']
 
 
 # 2. Once we have the readset ID,
 # lookup the reads at the position we are interested in
 request = service.reads().search(
-  body={'readsetIds': [na12878],
+  body={'readsetIds': [readset_id],
         'sequenceName': reference_name,
         'sequenceStart': reference_position,
         'sequenceEnd': reference_position,
@@ -86,6 +88,43 @@ reads = request.execute().get('reads', [])
 bases = [read['originalBases'][reference_position - read['position']]
          for read in reads]
 
-print 'NA12878 bases on %s at %d' % (reference_name, reference_position)
+print '%s bases on %s at %d are' % (sample, reference_name, reference_position)
 for base, count in Counter(bases).items():
   print '%s: %s' % (base, count)
+
+
+#
+# This example gets the variants for a sample at a specific position
+#
+
+# 1. First find the call set ID for the sample
+request = service.callsets().search(
+  body={'variantSetIds': [dataset_id], 'name': sample},
+  fields='callSets(id)')
+resp = request.execute()
+call_sets = resp.get('callSets', [])
+if len(call_sets) != 1:
+  raise Exception('Searching for %s didn\'t return '
+                  'the right number of call sets' % sample)
+
+call_set_id = call_sets[0]['id']
+
+
+# 2. Once we have the call set ID,
+# lookup the variants that overlap the position we are interested in
+request = service.variants().search(
+  body={'callSetIds': [call_set_id],
+        'referenceName': reference_name,
+        # Note: currently, variants are 0-based and reads are 1-based,
+        # reads will move to 0-based coordinates in the next version of the API
+        'start': reference_position - 1,
+        'end': reference_position},
+  fields='variants(names,referenceBases,alternateBases,calls(genotype))')
+variant = request.execute().get('variants', [])[0]
+
+variant_name = variant['names'][0]
+genotype = [variant['referenceBases'] if g == 0
+            else variant['alternateBases'][g - 1]
+            for g in variant['calls'][0]['genotype']]
+
+print 'the called genotype is %s for %s' % (','.join(genotype), variant_name)
