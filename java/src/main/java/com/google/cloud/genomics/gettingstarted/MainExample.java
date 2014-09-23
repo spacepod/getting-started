ba3de3a35f27c3bee4cb303e556f562f9d747139
cassiedoll <cassandradoll@gmail.com>
2014-09-23 15:58:34 -0700

    Add variant calling example

--- java/src/main/java/com/google/cloud/genomics/gettingstarted/MainExample.java
+++ java/src/main/java/com/google/cloud/genomics/gettingstarted/MainExample.java
@@ -24,11 +24,16 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.extensions.java6.auth.oauth2.GooglePromptReceiver;
 import com.google.api.services.genomics.Genomics;
 import com.google.api.services.genomics.GenomicsScopes;
+import com.google.api.services.genomics.model.CallSet;
 import com.google.api.services.genomics.model.Read;
 import com.google.api.services.genomics.model.Readset;
+import com.google.api.services.genomics.model.SearchCallSetsRequest;
 import com.google.api.services.genomics.model.SearchReadsRequest;
 import com.google.api.services.genomics.model.SearchReadsetsRequest;
+import com.google.api.services.genomics.model.SearchVariantsRequest;
+import com.google.api.services.genomics.model.Variant;
 import com.google.cloud.genomics.utils.GenomicsFactory;
+import com.google.common.base.Joiner;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.Lists;
 
@@ -83,31 +88,33 @@ public class MainExample {
 
 
       //
-      // This example gets the read bases for NA12878 at specific a position
+      // This example gets the read bases for a sample at specific a position
       //
       String datasetId = "376902546192"; // This is the 1000 Genomes dataset ID
+      String sample = "NA12872";
       String referenceName = "22";
-      final Integer referencePosition = 51005354;
+      final Integer referencePosition = 51003836;
 
-      // 1. First find the readset ID for NA12878
+      // 1. First find the readset ID for the sample
       SearchReadsetsRequest readsetsReq = new SearchReadsetsRequest()
           .setDatasetIds(Lists.newArrayList(datasetId))
-          .setName("NA12878");
+          .setName(sample);
 
       List<Readset> readsets = genomics.readsets().search(readsetsReq)
           .setFields("readsets(id)").execute().getReadsets();
       if (readsets == null || readsets.size() != 1) {
-        System.err.println("Searching for NA12878 didn't return the right number of results");
+        System.err.println("Searching for " + sample
+            + " didn't return the right number of readsets");
         return;
       }
 
-      String na12878 = readsets.get(0).getId();
+      String readsetId = readsets.get(0).getId();
 
 
       // 2. Once we have the readset ID,
       // lookup the reads at the position we are interested in
       SearchReadsRequest readsReq = new SearchReadsRequest()
-          .setReadsetIds(Lists.newArrayList(na12878))
+          .setReadsetIds(Lists.newArrayList(readsetId))
           .setSequenceName(referenceName)
           .setSequenceStart(BigInteger.valueOf(referencePosition))
           .setSequenceEnd(BigInteger.valueOf(referencePosition))
@@ -128,10 +135,60 @@ public class MainExample {
         baseCounts.put(base, baseCounts.get(base) + 1);
       }
 
-      System.out.println("NA12878 bases on " + referenceName + " at " + referencePosition);
+      System.out.println(sample + " bases on " + referenceName + " at "
+          + referencePosition + " are");
       for (Map.Entry<Character, Integer> entry : baseCounts.entrySet()) {
         System.out.println(entry.getKey() + ": " + entry.getValue());
       }
+
+
+      //
+      // This example gets the variants for a sample at a specific position
+      //
+
+      // 1. First find the call set ID for the sample
+      SearchCallSetsRequest callSetsReq = new SearchCallSetsRequest()
+          .setVariantSetIds(Lists.newArrayList(datasetId))
+          .setName(sample);
+
+      List<CallSet> callSets = genomics.callsets().search(callSetsReq)
+          .setFields("callSets(id)").execute().getCallSets();
+      if (callSets == null || callSets.size() != 1) {
+        System.err.println("Searching for " + sample
+            + " didn't return the right number of call sets");
+        return;
+      }
+
+      String callSetId = callSets.get(0).getId();
+
+
+      // 2. Once we have the call set ID,
+      // lookup the variants that overlap the position we are interested in
+      SearchVariantsRequest variantsReq = new SearchVariantsRequest()
+          .setCallSetIds(Lists.newArrayList(callSetId))
+          .setReferenceName(referenceName)
+          // Note: currently, variants are 0-based and reads are 1-based,
+          // reads will move to 0-based coordinates in the next version of the API
+          .setStart(Long.valueOf(referencePosition) - 1)
+          .setEnd(Long.valueOf(referencePosition));
+
+      Variant variant = genomics.variants().search(variantsReq)
+          .setFields("variants(names,referenceBases,alternateBases,calls(genotype))")
+          .execute().getVariants().get(0);
+
+      String variantName = variant.getNames().get(0);
+
+      List<String> genotype = Lists.newArrayList();
+      for (Integer g : variant.getCalls().get(0).getGenotype()) {
+        if (g == 0) {
+          genotype.add(variant.getReferenceBases());
+        } else {
+          genotype.add(variant.getAlternateBases().get(g - 1));
+        }
+      }
+
+      System.out.println("the called genotype is " + Joiner.on(',').join(genotype)
+          + " at " + variantName);
 
 
     } catch (ParameterException e) {
